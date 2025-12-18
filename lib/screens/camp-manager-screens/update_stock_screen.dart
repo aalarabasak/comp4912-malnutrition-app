e429fc4a4ba06bc376695d8afb4ad72b00eff746
Alara Basak <aalarabasak@gmail.com>
2025-12-18 22:22:02 +0300

    update stock form Ui is finished

--- lib/screens/camp-manager-screens/update_stock_screen.dart
+++ lib/screens/camp-manager-screens/update_stock_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+enum Stockcategory {rutf, supplement} //for segmented button widget
 
 class UpdateStockScreen extends StatefulWidget{
   const UpdateStockScreen({super.key});
@@ -9,25 +12,35 @@ class UpdateStockScreen extends StatefulWidget{
 
 class UpdateStockScreenState extends State<UpdateStockScreen>{
 
-  final formkey = GlobalKey<FormState>(); //to control form state of each box
+  Set<Stockcategory> selectedview = {Stockcategory.rutf}; //by default it is arranged as rutf
 
   // Controllers
-  final TextEditingController productnamecontroller = TextEditingController();
+  final TextEditingController supplementnamecontroller = TextEditingController();
   final TextEditingController lotnumbercontroller = TextEditingController();
   final TextEditingController quantitycontroller = TextEditingController();
   final TextEditingController expirydatecontroller = TextEditingController();
 
+  //will be deleted later, needs to be get from firebase
+  final List<String> rutfOptions = [
+    "Plumpy'Nut",
+    "Valid P-RUTF",
+    "eeZeePaste NUT",
+  ];
+
+  String? selectedrutfname; //chosen from dropdown menu
+  DateTime? selecteddate;//chosen expiry date for rutf
+
   @override
   void dispose() {
-    productnamecontroller.dispose();
+    supplementnamecontroller.dispose();
     lotnumbercontroller.dispose();
     quantitycontroller.dispose();
     expirydatecontroller.dispose();
     super.dispose();
   }
 
-  //for date picker logic for expiration date field
-  Future<void> pickexpirydate() async{
+  //for date picker logic for expiration date field for only RUTF 
+  Future<void> pickexpirydate(BuildContext context) async{
 
     final DateTime? picked = await showDatePicker(
       context: context, 
@@ -39,14 +52,25 @@ class UpdateStockScreenState extends State<UpdateStockScreen>{
 
     if (picked != null) {
       setState(() {
-        expirydatecontroller.text = '${picked.day}/${picked.month}/${picked.year}';
+        selecteddate = picked;
+        expirydatecontroller.text = DateFormat('dd/MM/yyyy').format(picked);
       });
     }
   }
   
+  void savestock(){
+
+
+  }
 
   @override
   Widget build(BuildContext context){
+
+    bool isrutfmode = false;
+    if(selectedview.first == Stockcategory.rutf){
+      isrutfmode = true;
+    }
+
     return Scaffold(
       appBar: AppBar(
       title: Icon(Icons.monitor_heart_outlined, color: Colors.black,),
@@ -58,118 +82,151 @@ class UpdateStockScreenState extends State<UpdateStockScreen>{
 
     body: GestureDetector(
       onTap: () {
-        //  to hide the keyboard
-          // It removes focus from the currently active text field
+        //If click on an empty space on the screen close the keyboard
         FocusScope.of(context).unfocus();
       },
-      child: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Form(
-            key: formkey,
+    child:SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+            
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              
 
               children: [
 
-                const SizedBox(height: 50),
-                //title
-                Text('Please fill in the details below.',
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold,)),
+                
+                const SizedBox(height: 8),
 
-                const SizedBox(height: 40),
+                //segmented buttonn
+                SizedBox(
+                  width: double.infinity,
+                  //I got the segmentenbutton code from below link:
+                  //https://api.flutter.dev/flutter/material/SegmentedButton-class.html
+                  child: SegmentedButton<Stockcategory>(
+                    showSelectedIcon: false,
+                    multiSelectionEnabled: false,
+                    emptySelectionAllowed: false,
+                    segments: const <ButtonSegment<Stockcategory>>[
+                      ButtonSegment<Stockcategory>(
+                        value: Stockcategory.rutf,
+                        label: Text("RUTF Items"),
+                        icon: Icon(Icons.medical_services_outlined),
+                      ),
+                      ButtonSegment<Stockcategory>(
+                        value: Stockcategory.supplement,
+                        label: Text("Supplements"),
+                        icon: Icon(Icons.apple, color: Color.fromARGB(255, 84, 84, 84),),
+                      ),
+                    ],
 
-                //the big container
+                    selected: selectedview,
+                    onSelectionChanged: (Set<Stockcategory> newselection) {
+                      setState(() {
+                        selectedview = newselection;
+                        lotnumbercontroller.clear();
+                        expirydatecontroller.clear();
+                        supplementnamecontroller.clear();
+                        quantitycontroller.clear();
+                        selectedrutfname = null;
+                      });
+                    },
+
+                    style: ButtonStyle(
+                      //I got the below code from below link for changing background of selection color
+                      //https://stackoverflow.com/questions/75271399/how-to-control-selected-and-unselected-background-color-for-the-segmentedbutton
+                      backgroundColor: MaterialStateProperty.resolveWith<Color?>((states) {
+                      if (states.contains(MaterialState.selected)) {
+                        return const Color.fromARGB(255, 229, 142, 171).withOpacity(0.5); //pink theme color
+                      }
+                      return null;//if it is not selected, then default one
+                    }),
+                    ),
+                  ),
+                ),
+               
+                
+                const SizedBox(height: 30),
+                
+                //dynamic form
                 Container(
-                  padding: const EdgeInsets.all(25),
+                  padding:const EdgeInsets.all(20) ,
                   decoration: BoxDecoration(
                     color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.1),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade200, blurRadius: 10, offset: const Offset(0, 5)
+                      )
+                    ]
                   ),
 
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+
+
                     children: [
-
-                      //1st input field-product name
-                      TextFormField(
-                        controller: productnamecontroller,
-                        decoration: InputDecoration(
-                          labelText: 'Product Name',
-                          labelStyle: TextStyle(fontSize: 14),
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.label_outline)
-                        ),
-
-                        validator: (value) => value!.isEmpty ? 'Required field.' : null,
+                      //title of the container based on segmented button selection
+                      Text(
+                        isrutfmode ? "RUTF Details" : "Supplement Details", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                       ),
 
-                      const SizedBox(height: 30),
+                    const Divider(),
+                    const SizedBox(height: 15),
 
-                      //2nd lot number
+                    //product name
+                    if(isrutfmode)...[
+                      DropdownButtonFormField<String>(
+                        value: selectedrutfname,
+                        decoration: inputdecoration("Select RUTF Product", Icons.medical_services),
+                        items: rutfOptions.map((name) => DropdownMenuItem(value: name, child: Text(name))).toList(),
+                        onChanged:(value) {
+                          setState(() {
+                            selectedrutfname = value;
+                          });
+                        },
+                      )
+                    ]
+                    else...[
                       TextFormField(
-                        controller: productnamecontroller,
-                        decoration: InputDecoration(
-                          labelText: 'Lot Number',
-                          labelStyle: TextStyle(fontSize: 14),
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.qr_code)
-                        ),
+                        controller: supplementnamecontroller,
+                        decoration: inputdecoration("Supplement Name", Icons.fastfood),
+                      )
+                    ],
 
-                        validator: (value) => value!.isEmpty ? 'Required field.' : null,
+                    const SizedBox(height: 20),
+
+                    //quantity -same in both selections
+                    TextFormField(
+                      controller: quantitycontroller,
+                      keyboardType: TextInputType.number,
+                      decoration: inputdecoration("Quantity", Icons.layers),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    //conditional field
+                    if(isrutfmode)...[
+                      //lot nuumber field
+                      TextFormField(
+                        controller: lotnumbercontroller,
+                        decoration: inputdecoration("Lot Number", Icons.qr_code),
                       ),
+                      const SizedBox(height: 20),
 
-                      const SizedBox(height: 30),
-
-                      //3rd quantity
-                      TextFormField(
-                          controller: quantitycontroller,
-                          keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                          decoration: const InputDecoration(
-                            labelText: 'Quantity',
-                            labelStyle: TextStyle(fontSize: 14),
-                            border: OutlineInputBorder(),
-                            prefixIcon: Icon(Icons.add_shopping_cart),
-                          ),
-                          validator: (value) {
-                            //check control
-                            if (value == null || value.isEmpty) return 'Required field.';
-                            if (int.tryParse(value) == null) return 'Invalid number.';
-                            return null;
-                          },
-                        ),
-
-                      const SizedBox(height: 30),
-
-                      //4th expiration date
+                      //expiration date
                       TextFormField(
                         controller: expirydatecontroller,
                         readOnly: true,
-                        onTap: pickexpirydate,
-                        decoration: InputDecoration(
-                          labelText: 'Expiration Date',
-                          labelStyle: TextStyle(fontSize: 14),
-                            border: OutlineInputBorder(),
-                            prefixIcon: Icon(Icons.calendar_month_outlined),
-                            hintText: 'Tap to select date..',
-                        ),
-                        validator: (value) => value!.isEmpty ? 'Required field.' : null,
-                      ),
+                        onTap: () => pickexpirydate(context),
+                        decoration: inputdecoration("Expiration Date", Icons.calendar_today),
+                      )
+                    ],
 
-                      
+
                     ],
                   ),
                 ),
 
-                const SizedBox(height: 50),
+                const SizedBox(height: 30),
 
                 //buttons row
                 Row(
@@ -205,9 +262,9 @@ class UpdateStockScreenState extends State<UpdateStockScreen>{
                           )
                         ),
                         onPressed:() {
-                          //will be updated!!1
+                          savestock();
                         }, 
-                        child: const Text('Save')),
+                        child: const Text("Save")),
                       ),
                   ],
                 )
@@ -216,10 +273,21 @@ class UpdateStockScreenState extends State<UpdateStockScreen>{
 
               ],
             )
-          ),
+          
         )
-      ),
-    ),
+        )
+     
+    );
+  }
+
+  InputDecoration inputdecoration(String label, IconData icon){
+    return InputDecoration(
+      labelText: label,
+      prefixIcon: Icon(icon, color: Colors.grey.shade600,),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      filled: true,
+      fillColor: Colors.grey.shade50,
+
     );
   }
 }
